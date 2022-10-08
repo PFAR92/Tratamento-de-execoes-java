8a1cdc4a27396ac7b4796001f78880b7cf150b53
PFAR92 <paulofelipetj@hotmail.com>
2022-10-08 10:47:58 -0300

    08/10/22

--- Exercicio Checked Exeption/src/App.java
+++ Exercicio Checked Exeption/src/App.java
@@ -2,14 +2,18 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 import java.io.OutputStreamWriter;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        String nomeDoArquivo = "romances-blake-crouch.txt";
+        imprimirArquivoNoConsole(nomeDoArquivo);
+
+        System.out.println("Apesar da exception ou não o arquivo continua");
     }
 
-    public static void imprimirArquivoNoConsole(String nomeDoArquivo) {
+    public static void imprimirArquivoNoConsole(String nomeDoArquivo) throws IOException {
         File file = new File(nomeDoArquivo);
         
         BufferedReader br = new BufferedReader(new FileReader(file.getName()));
