11b58744743afcc9af620f2b2126419c476c28cb
PFAR92 <paulofelipetj@hotmail.com>
2022-10-08 19:08:09 -0300

    08/10/22

--- Exercicio Checked Exeption/src/App.java
+++ Exercicio Checked Exeption/src/App.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
@@ -10,25 +9,17 @@ import javax.swing.JOptionPane;
 
 public class App {
     public static void main(String[] args) {
-        String nomeDoArquivo = "romances-blake-crouch.txt";
-        try {
-            imprimirArquivoNoConsole(nomeDoArquivo);
-        }catch(FileNotFoundException e){
-            e.printStackTrace();
-            JOptionPane.showMessageDialog(null, "Revise o nome do arquivo que você deseja imprimir");
-        } catch (IOException e) {
-            e.printStackTrace();
-            JOptionPane.showMessageDialog(null, "Ocorreu um erro inesperado! entre em contato com o suporte");
-        }
+        String nomeDoArquivo = JOptionPane.showInputDialog("Nome do arquivo a ser exibido");
 
+        imprimirArquivoNoConsole(nomeDoArquivo);
+        System.out.println("Apesar da exception ou não o arquivo continua");
     }
 
     public static void imprimirArquivoNoConsole(String nomeDoArquivo) throws IOException {
-        File file = new File(nomeDoArquivo);
         
-        BufferedReader br = new BufferedReader(new FileReader(file.getName()));
+        try{
+        BufferedReader br = lerArquivo(nomeDoArquivo);
         String line = br.readLine();
-
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         do{
@@ -36,8 +27,16 @@ public class App {
             bw.newLine();
             line=br.readLine();
         } while(line != null);
-
         bw.flush();
         bw.close();
+        } catch(IOException ex) {
+            JOptionPane.showMessageDialog(null, "Ocorreu um erro não esperado, fale com o suporte");
+            ex.printStackTrace();
+        }
+    }
+
+    public static BufferedReader lerArquivo(String nomeDoArquivo){
+        File file = new File(nomeDoArquivo);
+        return new BufferedReader(new FileReader(file));
     }
 }
